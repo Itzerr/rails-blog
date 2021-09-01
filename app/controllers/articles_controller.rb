e90b4d55dfbbe7dc0d38cbacc725ae6bd52638e5
Itzerr <eatzer@gmail.com>
2021-09-02 01:18:17 +0200

    basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: 'Itzerr', password: 'lolcat', except: [:index, :show]
+
   def index
     @articles = Article.all
   end
